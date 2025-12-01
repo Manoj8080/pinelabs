8e7a93b28843e4cd746f3d4748606a20a4e441b8
Manoj8080 <uisloading@gmail.com>
2025-12-01 16:46:39 +0530

    initial commit

--- pinelabs/build.gradle.kts
+++ pinelabs/build.gradle.kts
@@ -1,12 +1,13 @@
 plugins {
-    alias(libs.plugins.android.application)
+//    alias(libs.plugins.android.application)
+    id("com.android.library")
 }
 
 android {
     namespace = "com.nukkadshops.pinelabs"
     compileSdk = 36
 
-    defaultConfig {
+    /*defaultConfig {
         applicationId = "com.nukkadshops.pinelabs"
         minSdk = 24
         targetSdk = 36
@@ -14,7 +15,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
+    }*/
 
     buildTypes {
         release {
@@ -40,6 +41,12 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
     testImplementation(libs.junit)
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito:mockito-core:5.12.0")
+    testImplementation("org.mockito:mockito-inline:5.2.0")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
