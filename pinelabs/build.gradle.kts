85d8c6c02a10b175e6bf46ca887249874ffb2998
Manoj8080 <uisloading@gmail.com>
2025-12-09 13:10:39 +0530

    maven

--- pinelabs/build.gradle.kts
+++ pinelabs/build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 }
 
-val releaseVersion = "1.0.4"
+val releaseVersion = "1.0.5"
 
 // -----------------------------------------------------
 // 1️⃣ ZIP PACKAGE FOR SONATYPE CENTRAL
